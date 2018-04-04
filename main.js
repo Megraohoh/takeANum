d0c5113b7512e88311700e056a1b71853bb8f352
Meghan Debity <mdebity@gmail.com>
2018-04-04 17:01:51 -0500

    added function and comments

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+
+// The function will increment a global variable by one each time it is invoked
+//Keeping this as a global var will "anchor" the counter to start at one but not repeat numerical values as bands are added 
+let bandNumber = 1
 // Your job is to write a function that accepts any band name as an argument. 
-// The function will increment a global variable by one each time it is invoked, 
+//argument name is bandName
+    //function expression
+const takeNumber = function (bandName) {
+    //variable is referencing global var, including a period, and ending with 
+    //placeholder for arguments listed in provided const(s)
+    let bandConcatenation = `${bandNumber}` + ". " + `${bandName}`;
+    //counter that takes global var
+    bandNumber ++;
+    return bandConcatenation; //return stops the interpreter from running code to express output from function
+    
+}
 // and return that number, and the band name concatenated together.
 
-// const bandNumber = 1
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
 
-// const takeNumber = function () {
-//     /*
-//         Write your awesome code here. See comments
-//         below for what should be returned.
-//     */
-// }
-
-// const scum = takeNumber("Galactic Scum")
-// console.log(scum)  // This should print "1. Galactic Scum" in the console
-
-// const under = takeNumber("Underdogs")
-// console.log(under)  // This should print "2. Underdogs" in the console
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
